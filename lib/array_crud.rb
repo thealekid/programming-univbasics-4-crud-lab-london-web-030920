b3b614a8acf0eb25fe2f7020802a0d7c3201bab3
Chuk Orakwusi <chukorakwusi@gmail.com>
2020-03-06 11:00:54 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -34,7 +34,8 @@ end
 
 def retrieve_element_from_index(array, index_number)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  array.index = [2]
+  array = [2]
+  
 end
 
 def retrieve_first_element_from_array(array)
