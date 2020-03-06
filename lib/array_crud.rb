a5388499b2789b554b9cea2605b2999867eded9c
Chuk Orakwusi <chukorakwusi@gmail.com>
2020-03-06 10:56:20 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,15 +7,13 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  arrays_cat = array.pop
-  p array
-  p arrays_cat
+  furniture = ["couch","table","drawers","bed"]
   furniture.push("arrays!")
   p furniture
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  furniture = ["couch","table","drawers","bed"]
   furniture.unshift("wow")
   p furniture
 end
