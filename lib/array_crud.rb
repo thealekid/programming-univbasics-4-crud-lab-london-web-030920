6caf204a3887ccf24e5a7ba1ca138d2f20e03269
Chuk Orakwusi <chukorakwusi@gmail.com>
2020-03-06 10:53:02 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -26,10 +26,7 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  furniture = ["couch","table","drawers","bed"]
-  couch = furniture.shift
-  p furniture
-  p couch
+  
 end
 
 def retrieve_element_from_index(array, index_number)
