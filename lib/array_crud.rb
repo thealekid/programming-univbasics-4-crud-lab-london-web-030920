e01a6ae092b69502c4453f611775af4d8862bae4
Chuk Orakwusi <chukorakwusi@gmail.com>
2020-03-06 10:33:12 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,6 +9,7 @@ end
 def add_element_to_end_of_array(array, element)
   furniture = ["couch","table","drawers","bed"]
   furniture.push("vase")
+  
 end
 
 def add_element_to_start_of_array(array, element)
